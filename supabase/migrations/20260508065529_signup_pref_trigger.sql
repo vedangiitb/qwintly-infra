eefd85c0016f4c4d846c1c8560c4f9c3a723a926
vedangiitb <vedangbaleiitb@gmail.com>
2026-05-08 12:27:05 +0530

    signup trigger to create user_pref entry

---
+++ supabase/migrations/20260508065529_signup_pref_trigger.sql
@@ -0,0 +1,18 @@
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = public
+as $$
+begin
+  insert into public.user_preferences(id)
+  values (new.id);
+
+  return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+after insert on auth.users
+for each row
+execute procedure public.handle_new_user();
